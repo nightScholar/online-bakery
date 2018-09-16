a24853714474b84ae58f07f4ba42da06af72d2aa
Tyree <22990609+nightScholar@users.noreply.github.com>
2018-09-16 14:42:22 -0500

    added eureka configuraiton to account-address microservice

--- applications/account-address-microservice/src/main/java/com/nightscholar/bakery/accountaddress/AccountAddressMicroserviceApplication.java
+++ applications/account-address-microservice/src/main/java/com/nightscholar/bakery/accountaddress/AccountAddressMicroserviceApplication.java
@@ -2,8 +2,11 @@ package com.nightscholar.bakery.accountaddress;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class AccountAddressMicroserviceApplication {
 
 	public static void main(String[] args) {
